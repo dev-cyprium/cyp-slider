266d9eec43e0066f941d85239cbcb5bedd1e731d
destroyergm <stefan_vg@hotmail.com>
2016-08-21 18:15:34 +0200

    Add plugin init function

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+cyp-slider is a simple and open soruce jQuery plugin for creating image galleries/sliders.
+
+
+Requirements:
+ - jQuery
+ - Image element with the class of `cyp-gallery`
+
--- src/cyp-slider.js
+++ src/cyp-slider.js
@@ -1,3 +1,29 @@
-var cypSlider = {
-	
-}
+(function($) {
+	/*
+		Error to check is jQuery loaded.
+		Print a error message to the console if jQuery isn't found.
+	*/
+	if($ === undefined) {
+		console.error("Error: jQuery not found. Did you forget to include it or include it?");
+		return;
+	}
+
+
+	/*
+		Plugin entry point and init function.
+	*/
+	$.fn.cypSlider = function( options ) {
+		// Use the default options params for options that are not passed in.
+		var settings = $.extend({
+			width: 600,
+			height: 300,
+			showDots: false,
+			animation: 'slide',
+			images: []
+		}, options);
+
+		
+	}
+
+
+})( (typeof(jQuery) !== 'undefined' ? jQuery : undefined) );
